db7cfb1447f5a7ad773cd4b37f27e5e6deeb50fd
CarolGMilano <109172126+CarolGMilano@users.noreply.github.com>
2022-08-05 19:49:06 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+const btnAvancar = document.getElementById('btn_avancar');
+const btnVoltar = document.getElementById('btn_voltar');
+const cartoes = document.querySelectorAll('.item_cartao');
+let cartaoAtual = 0;
+
+    function esconderCartaoSelecionado()
+    {
+        const cartaoSelecionado = document.querySelector('.selecionado');
+        cartaoSelecionado.classList.remove('selecionado');
+    }
+
+    function mostrarCartao(indiceCartao)
+    {
+        cartoes[indiceCartao].classList.add('selecionado');
+    }
+
+        btnAvancar.addEventListener('click', function(){
+
+            if(cartaoAtual === cartoes.length - 1) 
+            {
+                esconderCartaoSelecionado();
+                cartaoAtual=0;
+                mostrarCartao(cartaoAtual);
+                return;
+            }
+            console.log(cartaoAtual);
+            esconderCartaoSelecionado();
+
+            cartaoAtual++;
+            mostrarCartao(cartaoAtual);
+        });
+
+
+        btnVoltar.addEventListener('click', function(){
+
+            if(cartaoAtual === 0) 
+            {
+                esconderCartaoSelecionado();
+                cartaoAtual=5;
+                mostrarCartao(cartaoAtual);
+                return;
+            }
+
+            esconderCartaoSelecionado();
+
+            cartaoAtual--;
+            mostrarCartao(cartaoAtual);
+        });
+
